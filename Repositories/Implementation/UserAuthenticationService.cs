dac456b3641ecd1f425b74f9821a72baf617e35e
Daniel Serrano Serrano <98311363+dannyserranoserrano@users.noreply.github.com>
2024-05-31 09:30:15 +0200

    Subida 2

---
+++ Repositories/Implementation/UserAuthenticationService.cs
@@ -0,0 +1,58 @@
+using AppStore.Models.Domain;
+using AppStore.Models.DTO;
+using AppStore.Repositories.Abstract;
+using Microsoft.AspNetCore.Identity;
+
+namespace AppStore.Repositories.Implementation;
+
+public class UserAuthenticationService : IUserAuthenticationService
+{
+    private readonly UserManager<ApplicationUser> userManager;
+    private readonly SignInManager<ApplicationUser> signInManager;
+
+    public UserAuthenticationService(
+        UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager
+        )
+    {
+        this.userManager = userManager;
+        this.signInManager = signInManager;
+    }
+
+    public async Task<Status> LoginAsync(LoginModel login)
+    {
+        var status = new Status();
+        var user = await userManager.FindByNameAsync(login.Username!);
+
+        if (user is null)
+        {
+            status.StatusCode = 0;
+            status.Message = "User not found";
+            return status;
+        }
+
+        if (!await userManager.CheckPasswordAsync(user, login.Password!))
+        {
+            status.StatusCode = 0;
+            status.Message = "Invalid password";
+            return status;
+        }
+
+        var resultado = await signInManager.PasswordSignInAsync(user, login.Password!, true, false);
+
+        if (!resultado.Succeeded)
+        {
+            status.StatusCode = 0;
+            status.Message = "Invalid password";
+        }
+        status.StatusCode = 1;
+        status.Message = "Login successful";
+        return status;
+
+    }
+
+    public async Task LogoutAsync()
+    {
+        await signInManager.SignOutAsync();
+    }
+}
